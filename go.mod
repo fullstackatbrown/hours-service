8410702d31c3c06d4ce509c7f2cef4048cc3dd4b
Nathan Benavides-Luu <nate1299@me.com>
2022-08-27 00:06:39 -0400

    Add request logging

--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.19
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/nthnluu/aether v0.1.6 // indirect
+	github.com/nthnluu/aether v0.1.7 // indirect
 	golang.org/x/net v0.0.0-20220728211354-c7608f3a8462 // indirect
 	golang.org/x/sys v0.0.0-20220731174439-a90be440212d // indirect
 	golang.org/x/text v0.3.7 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	pb "github.com/fullstackatbrown/hours/pb/out"
 	"github.com/fullstackatbrown/hours/service"
 	"github.com/nthnluu/aether/pkg/server"
+	"log"
 )
 
 var (
@@ -23,5 +25,11 @@ func main() {
 
 	// Run the server.
 	server.RunOrExit(grpcServer, *port, func(b *server.ServerConfig) {
+		// Request logger
+		b.AddRequestInterceptor(func(ctx context.Context, i interface{}) error {
+			methodName := server.GetFullMethodNameFromContext(ctx)
+			log.Printf("method:\"%v\" %v\n", methodName, i)
+			return nil
+		})
 	})
 }
